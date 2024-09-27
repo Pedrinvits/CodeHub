27ad5333830f2ffb3254282b9241377517ab8f9f
Pedro Mantovani <pedromantovani26@gmail.com>
2024-09-27 11:48:16 -0300

    feat : add session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import { auth } from "../../auth";
+import { Session_Provider } from "./auth/session-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -15,28 +18,32 @@ export const metadata: Metadata = {
   description: "Seu hub de noticias",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth()
   return (
-    <html lang="pt-br" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-      </body>
-    </html>
+    <Session_Provider  session={session}>
+      <html lang="pt-br" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+        </body>
+      </html>
+    </Session_Provider>
   );
 }
