e61895b3f4c8fd375f2621f29972b247d9271c8d
Pedro Mantovani <pedromantovani26@gmail.com>
2024-10-03 11:20:40 -0300

    feat : add fallback message when no users are found in the list

--- src/components/sugested-users/index.tsx
+++ src/components/sugested-users/index.tsx
@@ -4,30 +4,39 @@ import FollowButton from "../follow-button";
 import UnfollowButton from "../unfollow-button";
 
 type SuggestedUsersProps = {
-    list : {
-        isFollowed: string;
-        id: string;
-        name: string;
-    }
+    list : [
+        isFollowed: string,
+        id: string,
+        name: string,
+    ],
+    current_user_id : string
 }
-const SuggestedUsers = async ({ list, current_user_id }: any) => {
-    // console.log(list);
-    
+const SuggestedUsers = async ({ list, current_user_id }: SuggestedUsersProps) => {
+
     return (
         <>
             <h2 className="text-xl font-semibold mt-6 mb-4">Suggested Users</h2>
             <ul className="space-y-4">
-                {list?.map((list : any) => (
-                    <li key={list.id} className="flex items-center gap-2">
-                        <Avatar>
-                            <AvatarImage src={`/placeholder.svg?height=32&width=32`} alt={`list ${list}'s avatar`} />
-                            <AvatarFallback>U</AvatarFallback>
-                        </Avatar>
-                        <span>{list.name}</span>
-                        {list.isFollowed ? <UnfollowButton followerId = {current_user_id} followingId = {list.id} /> : <FollowButton followerId = {current_user_id} followingId = {list.id}/>}
-                    
-                    </li>
-                ))}
+            {list?.filter((listItem: any) => listItem.id !== current_user_id).length > 0 ? (
+                list
+                    ?.filter((listItem: any) => listItem.id !== current_user_id)
+                    .map((listItem: any) => (
+                        <li key={listItem.id} className="flex items-center gap-2">
+                            <Avatar>
+                                <AvatarImage src={`/placeholder.svg?height=32&width=32`} alt={`${listItem.name}'s avatar`} />
+                                <AvatarFallback>U</AvatarFallback>
+                            </Avatar>
+                            <span>{listItem.name}</span>
+                            {listItem.isFollowed ? (
+                                <UnfollowButton followerId={current_user_id} followingId={listItem.id} />
+                            ) : (
+                                <FollowButton followerId={current_user_id} followingId={listItem.id} />
+                            )}
+                        </li>
+                    ))
+            ) : (
+                <p className="text-sm text-gray-400">No users found</p>
+            )}
             </ul>
         </>
     );
