facffb39fbd84f6db15c083934f51d085d12a1f8
Pedro Mantovani <pedromantovani26@gmail.com>
2024-10-04 15:02:25 -0300

    fix : adicionando usuario na query de comentario

--- data/comments/add-comment.ts
+++ data/comments/add-comment.ts
@@ -1,23 +1,24 @@
-"use server"
+"use server";
 import { db } from "@/lib/db";
 
 export const createComments = async (
-
-    post_id: number, coment: string
-
+  post_id: number, 
+  coment: string, 
+  userId: number // Adiciona o userId como argumento
 ) => {
   try {
-
+ 
     const newComment = await db.coment.create({
-        data : {
-            coment,
-            post_id,
-        }
-    })
+      data: {
+        coment : coment,
+        post_id : post_id,
+        userId : userId, 
+      },
+    });
 
-    return { comment:newComment, success: "Comentário criado com sucesso!", error: null };
+    return { comment: newComment, success: "Comentário criado com sucesso!", error: null };
+
   } catch (error) {
-    console.error('Erro ao atualizar as informações:', error);
-    return { success: null, error: "Erro ao atualizar as informações." };
+    console.error('Erro ao criar comentário:', error);
   }
 };
