dd16e4104f0e37ad755738c95dcaf1f29dff2086
pedro <pedromantovani26@gmail.com>
2024-10-14 20:06:25 -0300

    fix : wrong side bar in dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,11 @@
 import { Navbar } from "@/components/navbar";
 import Side from "@/components/side";
+import { Sidebar } from "@/components/side/side";
 
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
      return (
           <div className="min-h-screen bg-background font-sans antialiased flex sm:flex-row flex-col">
-               <Side />
-               <Navbar title={""} />
+               <Sidebar />
                {children}
           </div>
      );
